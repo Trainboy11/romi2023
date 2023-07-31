9b04f4b6514d80a50ffdce2284c5327c65432ef0
Emerson W <emersonwicke85@gmail.com>
2023-07-31 19:14:11 -0400

    Initial Commit

---
+++ test/App.java/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int timeOfDay = 10;
+
+        if (timeOfDay == 10){
+            System.out.println("Hello, World!");
+      }
+    }
+}
